2090cec4a3c0ef601d336dfe57502759a79994b4
crispybugak <rhylawd@gmail.com>
2025-03-13 09:34:47 +0900

    [Bronze V] Title: ??!, Time: 48 ms, Memory: 5372 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cs
@@ -0,0 +1,11 @@
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            Console.WriteLine(a+"??!");
+        }
+    }
+}
