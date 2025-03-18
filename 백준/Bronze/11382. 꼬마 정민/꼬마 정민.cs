4d45d13bf075c6327ea3034f3b1473ee309c0bab
crispybugak <rhylawd@gmail.com>
2025-03-18 19:39:35 +0900

    [Bronze V] Title: 꼬마 정민, Time: 52 ms, Memory: 5480 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] input = Console.ReadLine().Split();
+        long A = long.Parse(input[0]);
+        long B = long.Parse(input[1]);
+        long C = long.Parse(input[2]);
+
+        Console.WriteLine(A + B + C);
+    }
+}
